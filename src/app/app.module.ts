ab22916117531af37d1cf5f60f035af2189c1906
AndriyKlymash <andriyklymash2020@gmail.com>
2021-06-07 12:46:37 +0300

    practice_1_train

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+//lesson 1
+// Создать несколько массивов с объектами.
+// Создать для каждого из  массивов компонент, который отрисовывает все объекты.
+// для отрисовки каждого отдельного объекта создать свой компонент.
+// Вывести все объекты в UI
+//
+//выводите всех юзеров, под каждым юзером его посты (все разделяете на компоненты, делаете сервисы и интерфейсы)
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
